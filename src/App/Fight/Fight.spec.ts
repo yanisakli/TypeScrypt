1f01421d5834a8b31d8c667dc39e0eb31fc428c5
Yanis <yanisakli@yahoo.fr>
2020-03-30 16:01:32 +0200

    :wrench: fix bug

--- src/App/Fight/Fight.spec.ts
+++ src/App/Fight/Fight.spec.ts
@@ -1,11 +1,16 @@
-import { Pikachu, Ditto } from "../../Pokemons";
 import { Pokemon } from "../Pokemon/Pokemon";
 import { Battle } from "./Fight";
 
 
 describe('It should be Pikachu', () => {
-    const pikachu: Pokemon = Pikachu;
-    const ditto: Pokemon = Ditto;
+    const pikachu: Pokemon = new Pokemon({
+        name: 'Pikachu',
+        speed: 90,
+    });
+    const ditto: Pokemon = new Pokemon({
+        name: 'Ditto',
+        speed: 48,
+    });
 
     test('should pick pikachu', () => {
         expect(Battle.fight(ditto, pikachu)).toBe(pikachu);
