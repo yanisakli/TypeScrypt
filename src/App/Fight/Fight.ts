898d45063db764682b83797aed56c64116a30ce1
Yanis <yanisakli@yahoo.fr>
2020-03-30 17:20:33 +0200

    :wrench: Add fight

--- src/App/Fight/Fight.ts
+++ src/App/Fight/Fight.ts
@@ -6,14 +6,40 @@ function randomInt(numberMax: number): number {
 }
 
 export class Battle{
+
+    static intervalId: NodeJS.Timeout;
+
     static fight(pokemon1: Pokemon, pokemon2: Pokemon): Pokemon {
         if(pokemon1.speed > pokemon2.speed){
             return pokemon1;
         } else if (pokemon2.speed > pokemon2.speed) {
-            return pokemon1;
+            return pokemon2;
         } else {
             const rand = randomInt(2);
             return rand === 0 ? pokemon1 : pokemon2;
         }
     }
+
+    /*static simulateFight(pokemon1: Pokemon, pokemon2: Pokemon): Promise<Pokemon> {
+        return new Promise((resolve, reject) => {
+            Battle.intervalId = setInterval(() => {
+                console.log("Start combat");
+                const roundOrder = Battle.fight(pokemon1, pokemon2);
+                console.log(`${roundOrder[0].name} start`);
+                roundOrder[0].attack(roundOrder[1], 0, roundOrder[0].checkHit(roundOrder[0].attacks[0]));
+                if (roundOrder[1].hp <= 0){
+                    resolve(roundOrder[0]);
+                    clearInterval(Battle.intervalId);
+                    return;
+                }
+                roundOrder[1].attack(roundOrder[0], 0, roundOrder[1].checkHit(roundOrder[1].attackList[0]));
+                if (roundOrder[0].hp <= 0){
+                    resolve(roundOrder[1]);
+                    clearInterval(Battle.intervalId);
+                    return;
+                }
+            }, 500 ,pokemon1, pokemon2);
+        })
+    }
+    */
 }
